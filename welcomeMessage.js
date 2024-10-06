b753043ac06b192b1af0caaf7566c26ad87453e1
yogik23 <61941105+yogik23@users.noreply.github.com>
2024-10-06 20:00:39 +0700

    Create welcomeMessage.js

---
+++ welcomeMessage.js
@@ -0,0 +1,19 @@
+
+const welcomeMessage = `
+
+
+███████╗██╗  ██╗██╗    ██╗
+██╔════╝██║ ██╔╝██║    ██║
+███████╗█████╔╝ ██║ █╗ ██║
+╚════██║██╔═██╗ ██║███╗██║
+███████║██║  ██╗╚███╔███╔╝
+╚══════╝╚═╝  ╚═╝ ╚══╝╚══╝ 
+                          
+ASSISTER DAILY LOGIN by SKW AIRDROP HUNTER
+`;
+
+function displayWelcomeMessage() {
+  console.log(welcomeMessage);
+}
+
+module.exports = displayWelcomeMessage;
